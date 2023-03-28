1a24ce993e1031e53bcd3ad6dddc975a0675b61e
bimtaiht <43242009+bimtaiht@users.noreply.github.com>
2023-03-28 16:18:47 +0700

    overwrite project

---
+++ bimdev-revitapi-library-main/ElementFilter/0.Command/Command/TestCommand.cs
@@ -0,0 +1,85 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Utility;
+using System.Collections.Generic;
+using System.Linq;
+using Autodesk.Revit.UI;
+using Model.Entity;
+using Model.Form;
+using Model.Data;
+
+namespace Model.RevitCommand
+{
+    [Transaction(TransactionMode.Manual)]
+    public class TestCommand : RevitCommand
+    {
+        public override void Execute()
+        {
+            //Input : View,BuiltCategories
+            //Output : FilterElement
+
+            var elements = new FilteredElementCollector(doc, doc.ActiveView.Id).WhereElementIsNotElementType().ToList()
+                .Where(x => x.Category != null);
+
+            var catergories = elements.Select(x => x.Category).Distinct(new CategoryComparer());
+
+            var inputBuiltInCategories = new List<BuiltInCategory>
+            { BuiltInCategory.OST_StructuralFraming, BuiltInCategory.OST_StructuralColumns };
+
+            var filteredElements = elements.Where(x => inputBuiltInCategories.Contains((BuiltInCategory)x .Category.Id.IntegerValue));
+            //TaskDialog.Show("Revit", catergories.CombineString(x=> x.Name));
+
+            sel.SetElement(filteredElements);
+
+           //sel.SetElement(elements);
+
+
+        }
+    }
+    [Transaction(TransactionMode.Manual)]
+    public class TestCommand2 : RevitCommand
+    {
+       
+        public override void Execute()
+        {
+
+            var processor = new FilterElementProcessor
+            {
+                View = doc.ActiveView,
+                BuiltInCategories = new List<BuiltInCategory>
+            { BuiltInCategory.OST_StructuralFraming, BuiltInCategory.OST_StructuralColumns }
+               
+            };
+            var filterElements = processor.FilteredElements;
+
+            sel.SetElement(filterElements);
+
+        }
+    }
+
+    [Transaction(TransactionMode.Manual)]
+    public class TestCommand3 : RevitCommand
+    {
+        private FilterElementData data => FilterElementData.Instance;
+
+        public override void Execute()
+        {
+            var form = data.Form;   
+            form.ShowDialog();
+
+        }
+    }
+
+    public class CategoryComparer : IEqualityComparer<Category>
+    {
+        public bool Equals(Category x, Category y)
+        {
+            return x.Id == y.Id;
+        }
+
+        public int GetHashCode(Category obj)
+        {
+            return obj.Id.IntegerValue;
+        }
+    }
+}
---
+++ bimdev-revitapi-library-main/ElementFilter/0.Command/Model/Entity/Comparer/CategoryComparer.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entity
+{
+    public class CategoryComparer : IEqualityComparer<Category>
+    {
+        public bool Equals(Category x, Category y)
+        {
+            return x.Id == y.Id;
+        }
+
+        public int GetHashCode(Category obj)
+        {
+            return obj.Id.IntegerValue;
+        }
+    }
+}
